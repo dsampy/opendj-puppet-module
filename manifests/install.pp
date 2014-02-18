da6374314933656dea97f66acd4c10ed2b0261b7
Derrick Ampy <dsampy@gmail.com>
2014-02-17 20:26:27 -0500

    Updated Puppet install classes to use nexus repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,13 @@
 class opendj::install {
 	wget::fetch { "Download OpenDJ":
-	  source      => 'http://maven.forgerock.org/repo/releases/org/forgerock/opendj/opendj-server/2.6.0/opendj-server-2.6.0.zip',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/forgerock/opendj/opendj-server/2.6.0/opendj-server-2.6.0.zip',
 	  destination => '/tmp/opendj-server-2.6.0.zip',
 	  timeout     => 0,
 	  verbose     => false,
 	}
 
 	wget::fetch { "Download OpenDJ REST LDAP Gateway":
-	  source      => 'http://maven.forgerock.org/repo/releases/org/forgerock/opendj/opendj-rest2ldap-servlet/2.6.7/opendj-rest2ldap-servlet-2.6.7-servlet.war',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/forgerock/opendj/opendj-rest2ldap-servlet/2.6.7/opendj-rest2ldap-servlet-2.6.7-servlet.war',
 	  destination => '/tmp/opendj-rest2ldap-2.6.7.war',
 	  timeout     => 0,
 	  verbose     => false,
